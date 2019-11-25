a1730450dc240b0d1b49b11aa4b21097d462e2e9
mmartinal <manuelatleti@hotmail.com>
2019-11-25 17:39:14 +0100

    clase

--- minishell/myshell.c
+++ minishell/myshell.c
@@ -1,36 +1,52 @@
-/*
-* Dudas:
-* qué ocurre en line si tenemos más de una redirección del mismo tipo?
-* las redirecciones a partir de aquí se hacen con execlp, hijos y pipes, no?
-*
-*/
-
-
-
+#import <signal.h>
 #import <stdio.h>
 #import <stdlib.h>
+#import <sys/types.h>
+#import <sys/wait.h>
+#import <unistd.h> //execvp
 
 #import "parser.h"
 
 #define PROMPT = "msh> "
 
+void primero (tcommand command){
+
+}
+
+void medio (tcommand command){
+
+}
+
+void ultimo (tcommand command){
+
+}
+
+
 int main(){
 
-    //Declaración de variables
-    char buf[1024];
-    int control = 1;
-    tline * line;
+    	//Declaración de variables
+    	char buf[1024];
+    	int control = 1;
+   	tline * line;
 
-    //Obtención de línea
-    while (control != 0){
-        printf(PROMPT);
-        scanf("%s", buf);
-        line = tokenize(buf);
+    	//Obtención de línea
+    	while (control != 0){
+        	printf(PROMPT);
+        	scanf("%s", buf);
+        	line = tokenize(buf);
 
-        if (line == NULL) control = 0;
-        else printf("\n");
-    }
-    
-    
+        	if (line == NULL) control = 0;
+        	else printf("\n");
+    	}
 
+	//Segunda declaración de variables
+	int p1[2], p2[2], pid;
+
+	pipe(p1);
+	pipe(p2);
+
+	pid = fork();
+	if (pid == 0){ //primero
+		close(p1
+	}
 }
